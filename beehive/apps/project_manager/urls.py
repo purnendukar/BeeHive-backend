4a67c18c223cb3ed73d4bee898d1d4144af87b52
Purnendu Kar <purnendu.kar8@gmail.com>
2021-11-21 17:16:41 +0530

    feat: project member api endpoint

--- beehive/apps/project_manager/urls.py
+++ beehive/apps/project_manager/urls.py
@@ -3,11 +3,21 @@ from django.urls.conf import include
 
 from rest_framework.routers import DefaultRouter
 
-from apps.project_manager.apis import ProjectViewSet, SprintViewSet, TaskViewSet
+from apps.project_manager.apis import (
+    ProjectViewSet,
+    SprintViewSet,
+    TaskViewSet,
+    ProjectMemberViewSet,
+)
 
 
 default_router = DefaultRouter(trailing_slash=False)
 
+default_router.register(
+    r"project/(?P<project_id>[^/.]+)/member",
+    ProjectMemberViewSet,
+    basename="project_members",
+)
 default_router.register("project", ProjectViewSet, basename="project")
 default_router.register("sprint", SprintViewSet, basename="sprint")
 default_router.register("task", TaskViewSet, basename="task")
